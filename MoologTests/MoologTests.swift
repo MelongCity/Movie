62bb2049cb9bfb0f529a36a528e51e22a5bc943e
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-10-14 19:41:13 +0900

    [Add] 비동기 네트워크 코드 unitTest #35

--- MoologTests/MoologTests.swift
+++ MoologTests/MoologTests.swift
@@ -6,23 +6,49 @@
 //
 
 import XCTest
+@testable
+import Moolog
+import RxSwift
+import RxCocoa
 
 final class MoologTests: XCTestCase {
+    var disposeBag = DisposeBag()
+    var sut: NetworkType!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        print("Setup func 실행 🟢 ")
+        sut = NetworkManager()
+        disposeBag = DisposeBag()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        print("tearDown func 실행 🟢 ")
+        sut = nil
+        disposeBag = DisposeBag()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_Network_Async_Success() throws {
+        // 1) expectation 2) fulfill 3) wait
+        
+        let promise = expectation(description: "RxSwift - Single Network Test")
+        
+        sut.callRequest(router: .trendingTV, type: TrendingTVResponse.self)
+            .subscribe(with: self) { owner, result in
+                switch result {
+                case .success(let response):
+                    print(response.results)
+                    XCTAssertTrue(
+                        !response.results.isEmpty,
+                        "데이터 있음!"
+                    )
+                    promise.fulfill()
+                case .failure(let error):
+                    print(error)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        wait(for: [promise], timeout: 10)
     }
 
     func testPerformanceExample() throws {
